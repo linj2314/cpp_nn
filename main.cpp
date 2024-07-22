dc8113e9177e80737539f9cb8f4d05565a0cd23c
linj2314 <linj2314@gmail.com>
2024-07-22 00:15:42 -0400

    Added init function and reading in pixel data as well as shuffling the data

--- main.cpp
+++ main.cpp
@@ -1,5 +1,50 @@
-#include <iostream>
+#include "main.hpp"
+
+using namespace std;
+using namespace cv;
+
+void init(vector<pair<int, vector<int>>> & test) {
+    //reading in all pixel data from training jpg files
+    vector<string> simpson_characters = {"bart_simpson", 
+                                        "charles_montgomery_burns", 
+                                        "homer_simpson", 
+                                        "krusty_the_clown", 
+                                        "lisa_simpson", 
+                                        "marge_simpson", 
+                                        "milhouse_van_houten", 
+                                        "moe_szyslak", 
+                                        "ned_flanders", 
+                                        "principal_skinner"};
+    test.reserve(8000);
+    for (int i = 0; i < 10; i++) {
+        string cmd = "cd train && cd " + simpson_characters[i] + " && ls > ../../temp.txt";
+        system(cmd.c_str());
+
+        ifstream in = ifstream("temp.txt");
+        string file_name;
+        while (in >> file_name) {
+            vector<int> v(784, 0);
+            Mat image = imread("train/" + simpson_characters[i] + "/" + file_name, cv::IMREAD_GRAYSCALE);
+            
+            for (int j = 0; j < image.rows; j++) {
+                for (int k = 0; k < image.cols; k++) {
+                    v[j * 28 + k] = static_cast<int>(image.at<uchar>(j, k));
+                }
+            }
+            test.push_back(make_pair(i, v));
+        }
+    }
+
+    //shuffle training data so we can separate into batches
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine rng(seed);
+    shuffle(test.begin(), test.end(), rng);
+};
 
 int main() {
+    vector<pair<int, vector<int>>> test;
+
+    init(test);
+
     return 0;
 }
---
+++ main.hpp
@@ -0,0 +1,15 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <chrono>
+
+#endif
